ab2ca47bfbb0b91c9a17735b4fef30bbeed76c92
veera Sudhakar <veerasudhakar480@gmail.com>
2023-08-08 21:33:57 +0530

    commit

--- force-app/main/default/lwc/doctorRelatedAppointments/doctorRelatedAppointments.js
+++ force-app/main/default/lwc/doctorRelatedAppointments/doctorRelatedAppointments.js
@@ -4,7 +4,7 @@ import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
 import Id from '@salesforce/user/Id';
 import UserIDFIELD from '@salesforce/schema/User.Id';
 import getRelatedData from '@salesforce/apex/DoctorRelatedAppointments.getRelatedData';
-import filterRecordsByCriteria from '@salesforce/apex/SampleController.filterRecordsByCriteria';
+//import filterRecordsByCriteria from '@salesforce/apex/SampleController.filterRecordsByCriteria';
 const FIELDS = [
     // Add the necessary fields from the Appointment__c object that you want to display
     'Appointment__c.Id',
@@ -19,18 +19,31 @@ const FIELDS = [
     'Appointment__c.Contact__r.Name',
     'Appointment__c.Contact__r.Email',
 ];
+
+ 
+
 export default class DoctorRelatedAppointments extends LightningElement {
-  
- @track relatedData;
-  
- currentUserId
+
+   // @track relatedData;
+   @track filterCriteria = 'all';
+   @track initialLoad = true; 
+
+@track relatedData;
+  currentUserId
+
+ 
+
     @wire(CurrentPageReference)
     currentPageReference;
+
+ 
 
     @wire(getRecord, {recordId: Id,fields: [UserIDFIELD] })
       currentUserInfo({error, data}) {
         if (data) {
              this.currentUserId= data.fields.Id.value;            
+
+ 
 
             console.log('data.fields.Id.value',data.fields.Id.value)
         } else if (error) {
@@ -38,21 +51,38 @@ export default class DoctorRelatedAppointments extends LightningElement {
             console.error(error)
         }
       }
- 
-       @wire(getRelatedData, { userId: '$currentUserId' })
-       wiredRelatedData({ error, data }) {
-        if (data) {
-            console.log('data',this.relatedData)
-            this.relatedData = data;
-        } else if (error) {
-            // Handle error
-      }
-    }
-    filterCriteria;
-    @wire(filterRecordsByCriteria, { filterCriteria: '$filterCriteria' })
-    records;
 
-    handleFilterChange(event) {
+      handleFilterChange(event) {
         this.filterCriteria = event.target.value;
+        this.applyFilter();
     }
+
+    @wire(getRelatedData, { userId: '$currentUserId', filter: '$filterCriteria' })
+wiredRelatedData({ error, data }) {
+    if (data) {
+        this.relatedData = data;
+        if (this.initialLoad) {
+            this.initialLoad = false; // Set the flag to false after initial load
+        } else {
+            this.applyFilter(); // Apply filter when data is received or filter criteria changes
+        }
+        this.applyFilter(); // Apply filter when data is received or filter criteria changes
+    } else if (error) {
+        // Handle error
+    }
+}
+
+
+// filterCriteria;
+//     @wire(getRelatedData, { userId: '$currentUserId' })
+//     wiredRelatedData({ error, data }) {
+//         if (data) {
+//             console.log('data',this.relatedData)
+//             this.relatedData = data;
+//         } else if (error) {
+//             // Handle error
+//         }
+//     }
+
+
 }
